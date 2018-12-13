20a9659e1f79d5ff1b0a0495e000cd102f06eb6d
MarceArratiaF <marratiaf@hotmail.com>
2018-12-13 20:19:05 -0300

    cambio a data.js para leer json

--- src/data.js
+++ src/data.js
@@ -1,11 +1,21 @@
 // esta es una función de ejemplo
 // puedes ver como agregamos la función a nuestro objeto global window
-
-const example= () => {
- 
-
-  return 'example';  //prueba
-};
-
-
-window.example = example;
+const example = () => {
+  let xmlhttp = new XMLHttpRequest();
+  xmlhttp.open("GET", "./data/injuries/injuries.json", true);
+  xmlhttp.setRequestHeader("Content-Type", "text/plain;charset=UTF-8");
+  xmlhttp.setRequestHeader("Access-Control-Allow-Origin", "*");
+  xmlhttp.send();
+  xmlhttp.onload = function() {
+  let objeto = xmlhttp.responseText;
+  ordenar(objeto);
+  }
+  console.log("HOLS");
+  return 'example';
+  };
+  function ordenar(jsonObj)
+  {
+  let objJson=JSON.parse(jsonObj);
+console.log(objJson);
+  }
+  window.example = example;
