997aec89b93cd6651160fdb362805d40d1af92f4
MarceArratiaF <marratiaf@hotmail.com>
2019-01-06 22:09:40 -0300

    trabajando en data funcion para hacer calculo de media

--- data.js
+++ data.js
@@ -13,5 +13,67 @@ function filterListYearBussines(data,YearA) {
     resultDataYear=data.filter(function(fil){
         return (fil.Year.slice(0,4)===YearA);
     });
+    //stringify convierte en string una estructura de JSON
     return JSON.stringify(resultDataYear).split("Total_Injured_Persons");
+}
+//funcion para hacer la media por decadas
+function halfByDecade(arrayHalf){
+    //variables para identificar suma de todos los elementos con cifras en cada década
+    let decadeSumA = 0;
+    let decadeSumB = 0;
+    let decadeSumC = 0;
+    let decadeSumD = 0;
+    let decadeSumE = 0;
+    let decadeSumF = 0;
+    //variables para identificar cuántos elementos estaban contenidos en el año
+    let decadeCountA = 0;
+    let decadeCountB = 0;
+    let decadeCountC = 0;
+    let decadeCountD = 0;
+    let decadeCountE = 0;
+    let decadeCountF = 0;
+    //variable que guarda el resultado del cálculo de la década
+    let ArrayResultHalf = [];
+    for(let i=0;i<arrayHalf.length;i++){
+        let arrayCut=String(arrayHalf[i]).split(":");
+        //primera decada (1960-1970)
+        if(parseInt(arrayCut[0])>=1960 && parseInt(arrayCut[0])<=1970){
+            decadeSumA += parseInt(arrayCut[1]);
+            decadeCountA += parseInt(arrayCut[2]);
+        }
+        //segunda decada (1971-1980)
+        if(parseInt(arrayCut[0])>=1971 && parseInt(arrayCut[0])<=1980){
+            decadeSumB += parseInt(arrayCut[1]);
+            decadeCountB += parseInt(arrayCut[2]);
+        }
+        //tercera decada (1981-1990)
+        if(parseInt(arrayCut[0])>=1981 && parseInt(arrayCut[0])<=1990){
+            decadeSumC += parseInt(arrayCut[1]);
+            decadeCountC += parseInt(arrayCut[2]);
+        }
+        //cuarta decada (1991-2000)
+        if(parseInt(arrayCut[0])>=1991 && parseInt(arrayCut[0])<=2000){
+            decadeSumD += parseInt(arrayCut[1]);
+            decadeCountD += parseInt(arrayCut[2]);
+        }
+        //quinta decada (2001-2010)
+        if(parseInt(arrayCut[0])>=2001 && parseInt(arrayCut[0])<=2010){
+            decadeSumE += parseInt(arrayCut[1]);
+            decadeCountE += parseInt(arrayCut[2]);
+        }
+        //segunda decada (2011-2016)
+        if(parseInt(arrayCut[0])>=2011 && parseInt(arrayCut[0])<=2016){
+            decadeSumF += parseInt(arrayCut[1]);
+            decadeCountF += parseInt(arrayCut[2]);
+        }
+    }
+    //dejando resultado por decada según posición del array
+    ArrayResultHalf[0]=decadeSumA/decadeCountA;
+    ArrayResultHalf[1]=decadeSumB/decadeCountB;
+    ArrayResultHalf[2]=decadeSumC/decadeCountC;
+    ArrayResultHalf[3]=decadeSumD/decadeCountD;
+    ArrayResultHalf[4]=decadeSumE/decadeCountE;
+    ArrayResultHalf[5]=decadeSumF/decadeCountF;
+    //se envía return para mostrar en main
+    return ArrayResultHalf;
 }
--- mainn.js
+++
@@ -1,8 +0,0 @@
-
-
-
-document.getElementById("year").addEventListener("click",()=>{
-    document.getElementById("graphics").style.display="none";
-  document.getElementById("base").style.display="block";
-  
-})  
