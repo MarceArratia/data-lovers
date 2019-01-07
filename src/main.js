1319b6c9dbb6667e53d1a2bfac42b35b028b97d1
LDLRL <lomelek@gmail.com>
2019-01-07 02:03:06 -0300

    creacion de pagina quienes somos, link a la misma.

--- src/main.js
+++ src/main.js
@@ -357,10 +357,6 @@ console.log("ue suman en total "+varSum);
 
 
 
-document.getElementById("year").addEventListener("click",()=>{
-  document.getElementById("graphics").style.display="none";
-document.getElementById("base").style.display="block";
-
 
 
 
