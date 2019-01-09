132e097aba85c7be6e9b1de156c7b1ff0ea0c498
MarceArratiaF <marratiaf@hotmail.com>
2019-01-09 19:19:22 -0300

    test unitario

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,28 +1,31 @@
 global.window=global
+//variables de referencia de lo que se utilizará
 let assert = require("chai").assert;
 let mainTest=require('../src/data.js');
-let INJUERIES=require('../src/data/injuries/injuries');
-let dataTest=INJUERIES;
+let INJURIES=require('../src/data/injuries/injuries');
+let dataTest=INJURIES;
  /*eslint-disable*/
-describe('main', () => {
+ //describe lo que se va a hacer(describe)
+describe('comprueba que exista una función ordenar', () => {
     it('debería existir una funcion ordenar', () => {
       assert.exists(typeof mainTest.order, 'object');
     });
   });
-
-  describe('main', () => {
+  describe('comprueba que exista una función filtrar', () => {
+    //it muestra los pasos del test
     it('debería existir una funcion Filtrar', () => {
+      //assert variable global del chai, exists es una función que pregunta si existe algo
       assert.exists(typeof mainTest.filterListYearBussines, 'object');
     });
   });
-
-  describe('main', () => {
+   //pregunta por cálculo por década
+  describe('comprueba que exista una función filtrar', () => {
     it('debería existir una funcion Calcular', () => {
       assert.exists(typeof mainTest.halfByDecade, 'object');
-    });
+    }); 
   });
-
-  describe('main', () => {
+  //chequear que función retorne un dato
+  describe('comprueba que retorne información', () => {
   it("comprobando el retorno de halfByDecade using: assert.typeOf(value,'value'): ", function() {
     result   = halfByDecade(dataTest);
     assert.exists(result);
