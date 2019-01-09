65f40ab3a9cfca18be4820f79c5a6b6c3ad722ac
MarceArratiaF <marratiaf@hotmail.com>
2019-01-09 17:41:07 -0300

    resolviendo test globales y ordenando index

--- js/bootstrap.bundle.js
+++ js/bootstrap.bundle.js
@@ -1,8 +1,8 @@
 /*!
   * Bootstrap v4.2.1 (https://getbootstrap.com/)
   * Copyright 2011-2018 The Bootstrap Authors (https://github.com/twbs/bootstrap/graphs/contributors)
-  * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
-  */
+  * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)*/
+  /*eslint-disable*/
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports, require('jquery')) :
   typeof define === 'function' && define.amd ? define(['exports', 'jquery'], factory) :
