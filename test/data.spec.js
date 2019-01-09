bb8ffe5d268bd1277b06a6f7862b076fc56c7f5e
System Administrator <root@MacBook-Pro-de-Miguel.local>
2019-01-09 18:41:25 -0300

    funciones de testeo

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,14 +1,31 @@
 global.window=global
-global.chai=require("chai");
-require('../src/data.js');
-require('../src/main.js');
-
-
-
+let assert = require("chai").assert;
+let mainTest=require('../src/data.js');
+let INJUERIES=require('../src/data/injuries/injuries');
+let dataTest=INJUERIES;
+ /*eslint-disable*/
 describe('main', () => {
-  it('debería ser un objeto', () => {
-
-    assert.equal(typeof order, 'object');
-  
+    it('debería existir una funcion ordenar', () => {
+      assert.exists(typeof mainTest.order, 'object');
+    });
   });
+
+  describe('main', () => {
+    it('debería existir una funcion Filtrar', () => {
+      assert.exists(typeof mainTest.filterListYearBussines, 'object');
+    });
+  });
+
+  describe('main', () => {
+    it('debería existir una funcion Calcular', () => {
+      assert.exists(typeof mainTest.halfByDecade, 'object');
+    });
+  });
+
+  describe('main', () => {
+  it("comprobando el retorno de halfByDecade using: assert.typeOf(value,'value'): ", function() {
+    result   = halfByDecade(dataTest);
+    assert.exists(result);
+  });  
 });
+
