3d3ac83ab0aacac0578fdfb45ade73553ba6103b
MarceArratia <marratiaf@hotmail.com>
2019-01-16 23:19:51 -0300

    Leyendo archivo Json

--- src/json.js
+++
@@ -1,9 +0,0 @@
-
-function pruebap(){
-//fetch lee un archivo JSON, texto, imágenes.otros
-   fetch('data/injuries/injuries.json')//fetch dice donde está el archivo
-    .then(res => res.json())//promesa en res, implementa el JSON
-    .then(data => {//promesa si se cumple escribe contenido de JSON
-        console.log(data);
-    })
-} 
